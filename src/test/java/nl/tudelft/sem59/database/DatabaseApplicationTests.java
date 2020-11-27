15f401e5c5f7b15b495af852a6a3c4d888a7e212
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-11-27 14:34:53 +0100

    Added the database module.

---
+++ src/test/java/nl/tudelft/sem59/database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.sem59.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
