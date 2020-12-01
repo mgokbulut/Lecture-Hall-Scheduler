d8e5851e20decf7c474beca522a78ad18ad6655b
LMMilliken <l.m.milliken@student.tudelft.nl>
2020-12-01 11:58:29 +0100

    created tests for the entities package in the rules module

--- rules/src/main/java/nl/tudelft/unischeduler/rules/entities/Room.java
+++ rules/src/main/java/nl/tudelft/unischeduler/rules/entities/Room.java
@@ -1,10 +1,13 @@
 package nl.tudelft.unischeduler.rules.entities;
 
+import java.util.Objects;
+
 public class Room {
 
     private int id;
     private int capacity;
     private String name;
+
 
     /**
      * constructor for Room.
@@ -42,5 +45,24 @@ public class Room {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Room room = (Room) o;
+        return getId() == room.getId()
+                && getCapacity() == room.getCapacity()
+                && Objects.equals(getName(), room.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCapacity(), getName());
     }
 }
