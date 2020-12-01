ab573f92c9f3491442a5caea682d63639b5104c7
LMMilliken <l.m.milliken@student.tudelft.nl>
2020-12-01 13:11:51 +0100

    created package for the core functionality of the rules module

--- rules/src/main/java/nl/tudelft/unischeduler/rules/entities/Lecture.java
+++ rules/src/main/java/nl/tudelft/unischeduler/rules/entities/Lecture.java
@@ -3,7 +3,7 @@ package nl.tudelft.unischeduler.rules.entities;
 import java.sql.Time;
 import java.sql.Timestamp;
 
-public class Lecture {
+public class Lecture implements Comparable {
 
     private int id;
     private int attendance;
@@ -65,5 +65,10 @@ public class Lecture {
 
     public void setRoom(Room room) {
         this.room = room;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return this.startTime.compareTo(((Lecture) o).startTime);
     }
 }
