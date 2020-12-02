9a595c14f802ca25d2882d53fb43c4f3e47cdacf
byarar <B.O.Yarar@student.tudelft.nl>
2020-12-02 04:09:05 +0300

    Added API call to add lecture to a student schedule.

--- database/src/main/java/nl/tudelft/unischeduler/database/ScheduleController.java
+++ database/src/main/java/nl/tudelft/unischeduler/database/ScheduleController.java
@@ -1,15 +1,25 @@
 package nl.tudelft.unischeduler.database;
 
+import nl.tudelft.unischeduler.database.entities.Lecture;
+import nl.tudelft.unischeduler.database.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
+    @Autowired
+    private UserService studentService;
+    @Autowired
+    private LectureService lectureService;
 
+    @PutMapping(path = "/assignStudentToLecture/{studentId}/{lectureId}")
+    public @ResponseBody
+    void assignStudentToLecture(@PathVariable String studentId, @PathVariable Long lectureId){
+        User u = studentService.getUser(studentId);
+        Lecture l = lectureService.getLecture(lectureId);
+        u.getSchedule().getLectures().add(l);
+    }
 }
--- database/src/main/java/nl/tudelft/unischeduler/database/ScheduleService.java
+++ database/src/main/java/nl/tudelft/unischeduler/database/ScheduleService.java
@@ -9,5 +9,4 @@ public class ScheduleService {
 
     @Autowired
     private ScheduleRepository scheduleRepository;
-
 }
