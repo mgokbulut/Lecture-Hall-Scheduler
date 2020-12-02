049b40e2145e9c68d41c4e0ed615ce448f7e862f
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2020-12-02 23:07:17 +0100

    changed the Application to make it more consistent with the other modules

--- schedule-generate/src/main/java/nl/tudelft/unischeduler/schedulegenerate/Application.java
+++ schedule-generate/src/main/java/nl/tudelft/unischeduler/schedulegenerate/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScheduleGenerateApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ScheduleGenerateApplication.class, args);
+        Application.run(Application.class, args);
     }
 
 }
