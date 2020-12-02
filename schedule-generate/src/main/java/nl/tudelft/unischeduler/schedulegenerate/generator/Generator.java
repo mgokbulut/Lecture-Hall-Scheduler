0d28e5897ba324cd7685bf4efde2ea1d7a610cef
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2020-12-02 23:48:11 +0100

    progress on the first method

--- schedule-generate/src/main/java/nl/tudelft/unischeduler/schedulegenerate/generator/Generator.java
+++ schedule-generate/src/main/java/nl/tudelft/unischeduler/schedulegenerate/generator/Generator.java
@@ -1,8 +1,13 @@
 package nl.tudelft.unischeduler.schedulegenerate.generator;
 
+import nl.tudelft.unischeduler.schedulegenerate.entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import nl.tudelft.unischeduler.schedulegenerate.generator.ApiCommunicator;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 @Service
 public class Generator {
@@ -10,8 +15,17 @@ public class Generator {
   @Autowired
   private ApiCommunicator apiCommunicator;
 
-  public void scheduleGenerate() {
-
+  public void scheduleGenerate(Timestamp currentTime) {
+    Time window = new Time(24 * 10, 0, 0); // placeholder
+    ArrayList<Course> courses = apiCommunicator.getCourses();
+    ArrayList<Lecture> lectures = new ArrayList<Lecture>();
+    // populate the lectures array
+    for(int i = 0; i < courses.size(); i++) {
+      Course course = courses.get(i);
+      ArrayList<Lecture> toAdd =
+          apiCommunicator.getLecturesInCourse(course.getId(), currentTime, window);
+      lectures.addAll(toAdd);
+    }
   }
   private void createTimeTable() { // lectures
 
