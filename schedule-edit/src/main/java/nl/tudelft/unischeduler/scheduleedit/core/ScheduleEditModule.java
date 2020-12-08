e7f30edcf4d369db69963cef9bf290323794e147
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-12-08 19:46:57 +0100

    Adds an exception that will be thrown when the date until a teacher is sick comes before the current date.

--- schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/core/ScheduleEditModule.java
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/core/ScheduleEditModule.java
@@ -1,8 +1,24 @@
 package nl.tudelft.unischeduler.scheduleedit.core;
 
+import java.time.Clock;
 import java.time.LocalDate;
+import nl.tudelft.unischeduler.scheduleedit.exception.IllegalDateException;
 
 public class ScheduleEditModule {
+
+    private Clock clock;
+
+    public ScheduleEditModule(Clock clock) {
+        this.clock = clock;
+    }
+
+    public Clock getClock() {
+        return clock;
+    }
+
+    public void setClock(Clock clock) {
+        this.clock = clock;
+    }
 
     /**
      * Reports the teacher sick and notifies the students that their class will be cancelled.
@@ -10,7 +26,9 @@ public class ScheduleEditModule {
      * @param teacherNetId The netId of the teacher that is sick.
      * @param until The LocalDate until the teacher is sick (inclusive).
      */
-    public void reportTeacherSick(int teacherNetId, LocalDate until) {
-
+    public void reportTeacherSick(int teacherNetId, LocalDate until) throws IllegalDateException {
+        if (until.isBefore(LocalDate.now(clock))) {
+            throw new IllegalDateException("the supplied date is before the current date");
+        }
     }
 }
