1b33400934c056ad8b114a6af14a450dc97f3e4b
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-12-08 19:16:42 +0100

    Setup for the rest of the project.

---
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/core/ScheduleEditModule.java
@@ -0,0 +1,16 @@
+package nl.tudelft.unischeduler.scheduleedit.core;
+
+import java.time.LocalDate;
+
+public class ScheduleEditModule {
+
+    /**
+     * Reports the teacher sick and notifies the students that their class will be cancelled.
+     *
+     * @param teacherNetId The netId of the teacher that is sick.
+     * @param until The LocalDate until the teacher is sick (inclusive).
+     */
+    public void reportTeacherSick(int teacherNetId, LocalDate until) {
+
+    }
+}
---
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/services/DataBaseService.java
@@ -0,0 +1,11 @@
+package nl.tudelft.unischeduler.scheduleedit.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * This package acts as a placeholder until the communication Part is worked out.
+ */
+@Service
+public class DataBaseService {
+
+}
