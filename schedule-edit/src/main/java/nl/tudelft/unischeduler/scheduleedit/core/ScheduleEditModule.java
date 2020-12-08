ca72417cebe39a45aae41b1cd7661407c5b4b63d
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-12-09 00:46:06 +0100

    Added internal logic for handling the lecturer reporting sick.

--- schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/core/ScheduleEditModule.java
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/core/ScheduleEditModule.java
@@ -1,12 +1,18 @@
 package nl.tudelft.unischeduler.scheduleedit.core;
 
+import java.io.IOException;
 import java.time.Clock;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.Period;
+import nl.tudelft.unischeduler.scheduleedit.exception.ConnectionException;
 import nl.tudelft.unischeduler.scheduleedit.exception.IllegalDateException;
+import nl.tudelft.unischeduler.scheduleedit.services.DataBaseService;
 
 public class ScheduleEditModule {
 
     private Clock clock;
+    private DataBaseService dataBaseService;
 
     public ScheduleEditModule(Clock clock) {
         this.clock = clock;
@@ -20,15 +26,31 @@ public class ScheduleEditModule {
         this.clock = clock;
     }
 
+    public DataBaseService getDataBaseService() {
+        return dataBaseService;
+    }
+
+    public void setDataBaseService(DataBaseService dataBaseService) {
+        this.dataBaseService = dataBaseService;
+    }
+
     /**
      * Reports the teacher sick and notifies the students that their class will be cancelled.
      *
      * @param teacherNetId The netId of the teacher that is sick.
      * @param until The LocalDate until the teacher is sick (inclusive).
      */
-    public void reportTeacherSick(int teacherNetId, LocalDate until) throws IllegalDateException {
-        if (until.isBefore(LocalDate.now(clock))) {
+    public void reportTeacherSick(int teacherNetId, LocalDate until)
+            throws IllegalDateException, ConnectionException {
+        LocalDate now = LocalDate.now(clock);
+        if (until.isBefore(now)) {
             throw new IllegalDateException("the supplied date is before the current date");
+        }
+        Period duration = now.until(until);
+        try {
+            dataBaseService.cancelLectures(teacherNetId, duration);
+        } catch (IOException e) {
+            throw new ConnectionException("The connection with the database failed", e);
         }
     }
 }
--- schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/services/DataBaseService.java
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/services/DataBaseService.java
@@ -1,5 +1,7 @@
 package nl.tudelft.unischeduler.scheduleedit.services;
 
+import java.io.IOException;
+import java.time.Period;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class DataBaseService {
 
+    /**
+     * Marks all the lectures of the lecturer during the timeslot as online.
+     *
+     * @param lecturerNetId The netId of the lecturer for which to cancel the lecture.
+     * @param period The date period for which to cancel the lecture.
+     */
+    public void cancelLectures(int lecturerNetId, Period period) throws IOException {
+        //TODO: this is a stub, and in the future should actually send the data.
+    }
 }
