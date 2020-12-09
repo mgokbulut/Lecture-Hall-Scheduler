004df415710bd94e0923fa2a8ea020dd3af3c9e8
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2020-12-09 09:52:30 +0100

    added rest of skeleton methods, all basic functionality is here now

--- authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractor.java
+++ authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractor.java
@@ -45,4 +45,53 @@ public class SysInteractor {
     // TODO throw exception of invalid user type for this action
     return "";
   }
+
+  public String stateOfRuleFollowing(User user) {
+    if(checkFaculty(user)) {
+      // TODO poll rules module to know if we're following all the rules
+      // (not a must-have)
+    }
+    unauthorizedAccess();
+    return "";
+  }
+
+  public String courseInformation(User user, Course course) {
+    if(checkStaff(user)) {
+      // TODO poll database module to get course information and return it
+    }
+    unauthorizedAccess();
+    return "";
+  }
+
+  public String lectureInformation(User user, Course course, int lectureNumber) {
+    if(checkStaff(user)) {
+      // TODO poll database to find lecture information and return it
+    }
+    unauthorizedAccess();
+    return "";
+  }
+
+  
+
+  private boolean checkFaculty(User user) {
+    // first we need to check that the user is the correct role
+    if(user.getType() == User.ROLE_FAC_MEMBER) {
+      return true;
+    }
+    // endpoint should then return an error message
+    return false;
+  }
+
+  private boolean checkStaff(User user) {
+    // in here we check whether a user is a university employee
+    if(user.getType() == User.ROLE_FAC_MEMBER || user.getType() == User.ROLE_TEACHER) {
+      return true;
+    }
+    // endpoint should then return an error message
+    return false;
+  }
+
+  private void unauthorizedAccess() {
+    // TODO return an exception with message "unauthorized access"
+  }
 }
