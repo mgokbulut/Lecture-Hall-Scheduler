589ea1e7a4ee3aec968a85d72ae7cb3f943ee6a0
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2020-12-09 09:51:35 +0100

    added skeleton methods with descriptions for main user queries

--- authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractor.java
+++ authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractor.java
@@ -7,18 +7,42 @@ import nl.tudelft.unischeduler.authentication.user.User;
 import nl.tudelft.unischeduler.authentication.utilentities.Course;
 import nl.tudelft.unischeduler.authentication.utilentities.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SysInteractor {
 
-  public String addCourse(Course course) {
+  public String addCourse(User user, Course course) {
+    // first we need to check that the user is the correct role
+    if(checkFaculty(user)) {
+      // TODO add the course to the db-module
+    }
+    unauthorizedAccess();
     return "";
   }
 
-  public String addUser(User user) {
+  public String addUser(User admin, User userToBeAdded) {
+    if(checkFaculty(admin)) {
+      // TODO add userToBeAdded to the database
+    }
+    unauthorizedAccess();
     return "";
   }
 
-  public String addClassroom(Room room) { return ""; }
+  public String addClassroom(User user, Room room) {
+    if(checkFaculty(user)) {
+      // TODO add room to DB
+    }
+    unauthorizedAccess();
+    return "";
+  }
+
+  public String reportCorona(User user) {
+    if(user.getType() == User.ROLE_STUDENT || user.getType() == User.ROLE_TEACHER) {
+      // TODO report to schedule-edit module that user has corona
+    }
+    // TODO throw exception of invalid user type for this action
+    return "";
+  }
 }
