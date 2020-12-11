f03a85de7484d145d33d6a5eab8290a944b17e20
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2020-12-11 18:07:13 +0100

    checkstyle method changes

--- authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractController.java
+++ authentication/src/main/java/nl/tudelft/unischeduler/authentication/sysinteract/SysInteractController.java
@@ -1,8 +1,6 @@
 package nl.tudelft.unischeduler.authentication.sysinteract;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SysInteractController {
 
-  @Autowired
-  private SysInteractor sysInteractor;
+    @Autowired
+    private SysInteractor sysInteractor;
 
-  @PostMapping(path = "/system/add_course")
-  public String addCourse(@RequestBody SysInteract sysInteraction) {
-    // return sysInteractor.addCourse(sysInteraction.getUser(), sysInteraction.getCourse());
-    return null;
-  }
+    @PostMapping(path = "/system/add_course")
+    public String addCourse(@RequestBody SysInteract sysInteraction) {
+        // return sysInteractor.addCourse(sysInteraction.getUser(), sysInteraction.getCourse());
+        return null;
+    }
 
+    @PostMapping(path = "/system/add_user")
+    public String addUser(@RequestBody SysInteract sysInteraction) {
+        // return sysInteractor.addUser(sysInteraction.getUser(), sysInteraction.getCourse());
+        return null;
+    }
 }
