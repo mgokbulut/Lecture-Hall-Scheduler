b48e9aac752643f8edcb4922f0d876bc6ff4ac84
LMMilliken <l.m.milliken@student.tudelft.nl>
2020-12-14 17:49:04 +0100

    updateRules method now calls verifySchedule once the rules have been updated

--- rules/src/main/java/nl/tudelft/unischeduler/rules/controllers/UpdateController.java
+++ rules/src/main/java/nl/tudelft/unischeduler/rules/controllers/UpdateController.java
@@ -2,6 +2,7 @@ package nl.tudelft.unischeduler.rules.controllers;
 
 import java.io.IOException;
 import nl.tudelft.unischeduler.rules.core.RulesModule;
+import nl.tudelft.unischeduler.rules.entities.Lecture;
 import nl.tudelft.unischeduler.rules.entities.Ruleset;
 import nl.tudelft.unischeduler.rules.storing.RulesParser;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,15 @@ public class UpdateController {
     public void updateRules(@RequestBody Ruleset newRules) throws IOException {
         parser.writeRules(newRules);
         module.setRules(newRules);
+        Lecture[] lect = getLectures();
+        module.verifySchedule(lect);
         System.out.println("updated the rules: \n" + newRules);
+    }
+
+    public Lecture[] getLectures() {
+        Lecture[] ret = new Lecture[1];
+        ret[0] = new Lecture(1, 1, null, null);
+        return ret;
     }
 
 }
