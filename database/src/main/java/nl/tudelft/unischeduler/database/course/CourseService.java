12d2e0e8c6f50e44baa7af567acc86336d04a641
Kuba Tokarz <j.m.tokarz@student.tudelft.nl>
2020-12-15 19:00:50 +0100

    CourseCreate API call

--- database/src/main/java/nl/tudelft/unischeduler/database/course/CourseService.java
+++ database/src/main/java/nl/tudelft/unischeduler/database/course/CourseService.java
@@ -1,7 +1,10 @@
 package nl.tudelft.unischeduler.database.course;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
@@ -25,5 +28,21 @@ public class CourseService {
         return courseRepository
                 .findById(courseId)
                 .orElseThrow(() -> new IllegalArgumentException("No such object in DB"));
+    }
+
+    public ResponseEntity<?> createCourse(String name, Integer year){
+        try{
+            Optional<Course> courseOptional = courseRepository
+                    .findByAndNameAndYear(name, year);
+            if(courseOptional.isPresent()){
+                return ResponseEntity.notFound().build();
+            }
+            Course course = new Course(name, year);
+            courseRepository.save(course);
+            return ResponseEntity.ok(course.getId());
+        } catch (Exception a){
+            a.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
