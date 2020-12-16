4fcc1fb5530f9a156fdcbd0859327e3a49fa4601
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-12-16 09:56:28 +0100

    Adds a stub in order to fake the use of the databaseService.

---
+++ schedule-edit/src/main/java/nl/tudelft/unischeduler/scheduleedit/services/CourseService.java
@@ -0,0 +1,28 @@
+package nl.tudelft.unischeduler.scheduleedit.services;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ * This package acts as a placeholder until the communication Part is worked out.
+ */
+@Service
+public class CourseService {
+
+    /**
+     * Creates a course and marks it to be scheduled by the schedule generate module.
+     *
+     * @param name The name of the course.
+     * @param year The year the course is thought.
+     * @return The generated id of the course.
+     */
+    public long createCourse(String name, int year) {
+        //TODO: this is a stub, and in the future should actually send the data.
+        return -1;
+    }
+
+    public long addStudentToCourse(List<String> netIdList, long courseId) {
+        //TODO: this is a stub, and in the future should actually send the data.
+        return -1;
+    }
+}
