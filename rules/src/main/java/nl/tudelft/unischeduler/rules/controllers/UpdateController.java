6b7f1cdf1ec475f9695101d6973fda939d3bfc76
Jop Schaap <J.Schaap-1@student.tudelft.nl>
2020-12-18 04:19:35 +0100

    Fix bugs and make every controller work the way they are intended.

--- rules/src/main/java/nl/tudelft/unischeduler/rules/controllers/UpdateController.java
+++ rules/src/main/java/nl/tudelft/unischeduler/rules/controllers/UpdateController.java
@@ -3,12 +3,12 @@ package nl.tudelft.unischeduler.rules.controllers;
 import java.io.IOException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 import nl.tudelft.unischeduler.rules.core.RulesModule;
 import nl.tudelft.unischeduler.rules.entities.Lecture;
 import nl.tudelft.unischeduler.rules.entities.Ruleset;
 import nl.tudelft.unischeduler.rules.services.DatabaseService;
 import nl.tudelft.unischeduler.rules.storing.RulesParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UpdateController {
 
-    @NonNull private RulesParser parser;
-    @NonNull private RulesModule module;
-    @NonNull private DatabaseService databaseService;
+    @Autowired private RulesParser parser;
+    @Autowired private RulesModule module;
+    @Autowired private DatabaseService databaseService;
 
     /**
      * Updates the current rules file to the newRules passed by the body of the request.
