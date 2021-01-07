0943b9746c3b221a36ac671cb739114d36971dd1
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2021-01-07 21:31:34 +0100

    template for the student notifier

--- database/src/main/java/nl/tudelft/unischeduler/database/triggers/StudentNotifier.java
+++ database/src/main/java/nl/tudelft/unischeduler/database/triggers/StudentNotifier.java
@@ -1,2 +1,12 @@
-package nl.tudelft.unischeduler.database.triggers;public class StudentNotifier {
+package nl.tudelft.unischeduler.database.triggers;
+
+public class StudentNotifier extends UserNotifier {
+
+  @Override
+  // we're going to first implement being notified when a class is moved online
+  public boolean update(int lectureId, String action) {
+    
+    return false;
+  }
+
 }
