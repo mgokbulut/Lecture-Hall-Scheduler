a9a2cb2ff4a2f4cf73d23dad392abb61ee2cfd91
Nathan Axcan <N.A.OrdonezCardenas@student.tudelft.nl>
2021-01-08 21:05:04 +0100

    Publisher implemented

--- database/src/main/java/nl/tudelft/unischeduler/database/triggers/LecturePublisher.java
+++ database/src/main/java/nl/tudelft/unischeduler/database/triggers/LecturePublisher.java
@@ -1,2 +1,33 @@
-package nl.tudelft.unischeduler.database.triggers;public class LecturePublisher {
+package nl.tudelft.unischeduler.database.triggers;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class LecturePublisher {
+
+    private Set<LectureSubscriber> subscribers;
+
+    public LecturePublisher() {
+        this.subscribers = new HashSet<>();
+    }
+
+    public void addSubscriber(LectureSubscriber sub) {
+        if(!this.subscribers.contains(sub)) {
+            this.subscribers.add(sub);
+        }
+    }
+
+    public void removeSubscriber(LectureSubscriber sub) {
+        this.subscribers.remove(sub);
+    }
+
+    public void notifySubscribers(long lectureId, String[] action, String author) {
+        Iterator<LectureSubscriber> it = this.subscribers.iterator();
+        while(it.hasNext()) {
+            LectureSubscriber sub = it.next();
+            sub.update(lectureId, action, author);
+        }
+    }
+
 }
