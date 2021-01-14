3d5da4e1009455bf6d881dc979e23c0c88bfca8e
Mert <m.gokbulut@student.tudelft.nl>
2021-01-14 18:32:04 +0100

    add tests for gateway-authentication module

---
+++ gateway-authentication/src/test/java/nl/tudelft/unischeduler/Integration/TokenTests.java
@@ -0,0 +1,10 @@
+package nl.tudelft.unischeduler.authentication;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TokenTests {
+    @BeforeEach
+    public void setup() {
+
+    }
+}
---
+++ gateway-authentication/src/test/java/nl/tudelft/unischeduler/user/userTests.java
@@ -0,0 +1,2 @@
+package nl.tudelft.unischeduler.user;public class userTests {
+}
